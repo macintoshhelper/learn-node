243213e7c0a2064f6e2cbd0bb259dfb18af1fa7b
macintoshhelper <macintoshhelper@users.noreply.github.com>
2017-07-05 14:32:09 +0100

    Update express boilerplate app.js

--- express/examples/boilerplate/src/app.js
+++ express/examples/boilerplate/src/app.js
@@ -1,24 +1,26 @@
 const path = require('path');
 const express = require('express');
 
-const router = require('./routes/router');
+const router = require('./routes/router');  // Import router map (list of routes)
 
-const app = express();
+const app = express();  // Set app to express object
 
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views'));  // Set express to use the folder 'views' as the views directory
 
-app.set('view engine', 'hbs');
+app.set('view engine', 'hbs');  // Set express's view engine to 'hbs' (handlebars) - you can use other templating engines like Jade
 
-app.use(router);
+app.use(router);  // Tell Express to use the router (import the list of routes, and their handlers)
 
-app.use((req, res/*, next*/) => {
+
+// The below function will run if none of the routes in 'router' matched - so the route was not found
+app.use((req, res) => {
   const err = new Error('Error 404: Page not found');
   err.status = 404;
   res.status(err.status);
   res.send(err);
-  // next(err);
 });
 
+// Setup an error renderer
 app.use((err, req, res) => {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -27,4 +29,5 @@ app.use((err, req, res) => {
   res.render('error');
 });
 
+// Export app for other packages to use it
 module.exports = app;
